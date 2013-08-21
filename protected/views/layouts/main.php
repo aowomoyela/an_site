60d753f87a0b2e41b6ffc511a3fe7bde0a3aecd4
An <an@owomoyela.net>
2013-08-21 12:13:30 -0600

    Added a gitignore, Gii'd up an Anecdote model, added prelim support for sub-navigation menus.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -23,6 +23,14 @@
 		<?php $this->widget('zii.widgets.CMenu', SiteElement::get_main_nav_array()); ?>
 	</div><!-- mainmenu -->
 
+	<?php
+		if ( isset($secondary_navigation) ) {
+			echo '<div id="submenu">'."\r\n";
+			$this->widget('zii.widgets.CMenu', $secondary_navigation);
+			echo '</div>'."\r\n";
+		}
+	?>
+
 	<?php echo $content; ?>
 
 	<div class="clear"></div>
